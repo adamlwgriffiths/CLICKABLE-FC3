277fc49b81346ff4f88c7d0fff76d3f1b1c97d35
Adam Griffiths <adam.lw.griffiths@gmail.com>
2023-04-11 22:01:11 +1000

    Remove nag dialog
    The dialog is not only really annoying, if you die before you close it it will crash DCS.
    So let's remove it.
    
    The README on the repo has the original authors patreon and paypal details. This is sufficient for attribution and compensation.

--- Cockpit/Scripts/UTILS/utils.lua
+++ Cockpit/Scripts/UTILS/utils.lua
@@ -14,12 +14,11 @@ package.path 					=	package.path
 									.. './dxgui/skins/common/?.lua;'
 									.. './MissionEditor/modules/?.lua;'
 									.. './Mods/tech/CombinedArms/UI/?.lua;'
-local   	TIMER              	=   0
 local 		optionsEditor 		= 	require('optionsEditor')
 local 		update_time_step 	= 	5 
 local 		F15c_extended 		= 	get_plugin_option_value("RedK0d Clickable", "F15c_extended", "local")
 local 		version
-local 		aircraft 			= get_aircraft_type()
+local 		aircraft 			=   get_aircraft_type()
 function check_entry_version()
     local file = io.open(entry, 'r')
     local fileContent = {}
@@ -27,28 +26,26 @@ function check_entry_version()
         table.insert (fileContent, line)
     end
     io.close(file)
-     		version =string.match(fileContent[8],'............',15)
+     		version = string.match(fileContent[8],'............',15)
 	return 	version
 end
 function 				dev_install()
-dofile(LockOn_Options.script_path.."UTILS\\dev_install.lua")
+	dofile(LockOn_Options.script_path.."UTILS\\dev_install.lua")
 end
+
 check_entry_version()
 optionsEditor.setOption("plugins.RedK0d Clickable.Version",version)
 dev_install()
+
 function post_initialize()
-		TIMER = 20
-		print_message_to_user("[ RedK0d Clickable ] \n[ "..version.." ]\n[ " .. aircraft .." ]",10)
+		local message = "" ..
+			'[ RedK0d Clickable ]\n' ..
+			'[ ' .. version .. ' ]\n' ..
+			'[ ' .. aircraft .. ' ]\n'
+		print_message_to_user(message,10)
 end
+
 need_to_be_closed = false 
 
 function update()
-    if                  TIMER     >   0                                                               then
-                        TIMER     =   TIMER - update_time_step
-        if              TIMER     <=  0                                                               then
-                        TIMER     =   0
-						dofile(LockOn_Options.script_path.."UTILS\\show_support_window.lua")
-		end
-      
-    end
 end
